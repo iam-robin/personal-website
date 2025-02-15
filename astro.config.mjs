d11d5b803a34790ef766de2c07229817a1097e24
iam-robin <spielmannrobin@gmail.com>
2025-02-15 23:27:35 +0100

    wip: 365 rss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import icon from 'astro-icon';
-
 import react from '@astrojs/react';
 
 // https://astro.build/config
