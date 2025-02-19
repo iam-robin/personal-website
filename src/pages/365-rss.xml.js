8d91dd0ce5d36ce1fd1c69a3a9a8a9f5c3faa310
iam-robin <spielmannrobin@gmail.com>
2025-02-19 22:59:45 +0100

    add copy photos to public script

--- src/pages/365-rss.xml.js
+++ src/pages/365-rss.xml.js
@@ -8,10 +8,20 @@ export async function GET(context) {
         title: 'iamrobin 365 Project',
         description: 'A photo a day for a year',
         site: context.site,
+        xmlns: {
+            media: 'http://search.yahoo.com/mrss/'
+        },
         items: photoItems.map((item) => ({
             title: item.data.title,
             pubDate: item.data.pubDate,
-            description: item.data.description
+            description: item.data.description,
+            customData: `<media:content
+            type="image/jpeg"
+            width="200"
+            height="600"
+            medium="image"
+            url="${context.site + item.data.image}" />
+        `
         })),
         customData: `<language>en-us</language>`
     });
