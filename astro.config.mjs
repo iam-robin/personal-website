6ae3015a41fda3dc9b47e20edb616fd896e6231e
iam-robin <spielmannrobin@gmail.com>
2025-06-25 13:11:05 +0200

    change astro output to hybrid instead of server

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ import db from '@astrojs/db';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://iamrob.in',
-    output: 'server',
+    output: 'hybrid',
     adapter: vercel(),
     integrations: [tailwind(), icon(), react(), db()],
     markdown: {
