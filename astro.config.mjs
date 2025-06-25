b64700cf26928866eeb0f20440df14ac808b2a3f
iam-robin <spielmannrobin@gmail.com>
2025-06-25 12:20:09 +0200

    add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,15 @@ import tailwind from '@astrojs/tailwind';
 import icon from 'astro-icon';
 import react from '@astrojs/react';
 import remarkGfm from 'remark-gfm';
+import vercel from '@astrojs/vercel';
 
 import db from '@astrojs/db';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://iamrob.in',
+    output: 'server',
+    adapter: vercel(),
     integrations: [tailwind(), icon(), react(), db()],
     markdown: {
         remarkPlugins: [remarkGfm]
