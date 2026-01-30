125bf70d95a8d57ef7c7d522f5c43ccd2ea0f390
iam-robin <spielmannrobin@gmail.com>
2026-01-30 09:52:58 +0100

    fix og image url on production

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
+    site: "https://iamrob.in",
     integrations: [icon(), db()],
 
     vite: {
