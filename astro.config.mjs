378441698ede3afdf39cba2976bafefd02a61c63
iam-robin <spielmannrobin@gmail.com>
2026-03-20 16:08:41 +0100

    chore: swap to node adapter for hetzner

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,8 @@ import icon from "astro-icon";
 import db from "@astrojs/db";
 import tailwindcss from "@tailwindcss/vite";
 import { fileURLToPath } from "url";
+import node from "@astrojs/node";
 
-import vercel from "@astrojs/vercel";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
@@ -16,11 +16,11 @@ export default defineConfig({
         db(),
         sitemap({
             filter: (page) =>
-                !page.includes('/postcards/error') &&
-                !page.includes('/postcards/success') &&
-                !page.includes('/postcards/new') &&
-                !page.endsWith('/blog-rss.xml') &&
-                !page.endsWith('/bookmarks-rss.xml'),
+                !page.includes("/postcards/error") &&
+                !page.includes("/postcards/success") &&
+                !page.includes("/postcards/new") &&
+                !page.endsWith("/blog-rss.xml") &&
+                !page.endsWith("/bookmarks-rss.xml"),
         }),
     ],
 
@@ -28,15 +28,23 @@ export default defineConfig({
         plugins: [tailwindcss()],
         resolve: {
             alias: {
-                "@components": fileURLToPath(new URL("./src/components", import.meta.url)),
-                "@assets": fileURLToPath(new URL("./src/assets", import.meta.url)),
-                "@layouts": fileURLToPath(new URL("./src/layouts", import.meta.url)),
-                "@utils": fileURLToPath(new URL("./src/utils", import.meta.url)),
+                "@components": fileURLToPath(
+                    new URL("./src/components", import.meta.url),
+                ),
+                "@assets": fileURLToPath(
+                    new URL("./src/assets", import.meta.url),
+                ),
+                "@layouts": fileURLToPath(
+                    new URL("./src/layouts", import.meta.url),
+                ),
+                "@utils": fileURLToPath(
+                    new URL("./src/utils", import.meta.url),
+                ),
                 "@api": fileURLToPath(new URL("./src/api", import.meta.url)),
                 "@data": fileURLToPath(new URL("./src/data", import.meta.url)),
             },
         },
     },
 
-    adapter: vercel(),
+    adapter: node({ mode: "standalone" }),
 });
